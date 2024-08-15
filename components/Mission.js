9d249f5194b509b27b0d9665cf7ab73b79767a92
Dmitry Persiianov <persiyanov.d@gmail.com>
2024-08-15 19:21:00 -0400

    Modified mission statement

--- components/Mission.js
+++ components/Mission.js
@@ -11,9 +11,9 @@ const Mission = () => {
         priority={true}
       />
       <p className="text-xl text-slate-800 w-full lg:w-[750px] text-center font-light">
-        Our mission is to provide high-end and quality services with consistency
-        in appoach and creativity, while keeping up to date with techniques to
-        stay current and on trend.
+      Our mission is to enhance your beauty with personalized, expert care, 
+            creating a welcoming environment where your unique style is 
+            celebrated and every visit is a memorable experience.
       </p>
       <h3 className="text-xl text-slate-800 text-center w-full lg:w-[650px]">
         - Alexander Shefield & Freddy Costanolli, Co-founders
